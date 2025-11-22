d7852369483bb7ec2deb4d5d037b0b0532621894
hula <2026617199@qq.com>
2025-11-22 22:47:45 +0800

    重构首页组件，优化图片加载逻辑并添加模糊背景图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="home-container">
     <ImageViewer 
-      :image-src="homeImage"
+      :image-src="currentImage"
       image-alt="首页背景"
       :enable-inertia="true"
       :border-feedback="true"
+      :class="{ 'image-loaded': isHighQualityLoaded }"
     />
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
 import ImageViewer from '../components/ImageViewer.vue'
-import homeImage from '../png/首页.png'
+import lowQualityImage from '../png/首页-模糊.png'
+import highQualityImage from '../png/首页.png'
+
+const currentImage = ref(lowQualityImage)
+const isHighQualityLoaded = ref(false)
+
+onMounted(() => {
+  const img = new Image()
+  img.src = highQualityImage
+  
+  img.onload = () => {
+    currentImage.value = highQualityImage
+    isHighQualityLoaded.value = true
+  }
+})
 </script>
 
 <style scoped>
@@ -20,5 +36,9 @@ import homeImage from '../png/首页.png'
   height: 100%;
   overflow: hidden;
   position: relative;
+}
+
+.home-container :deep(.viewer-image) {
+  transition: opacity 0.5s ease-in-out;
 }
 </style>
