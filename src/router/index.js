92ec4ec6db3540c48a3dd0a174ad4f90bc9411d4
hula <2026617199@qq.com>
2025-11-26 09:17:50 +0800

    添加了黄山的热点图

--- src/router/index.js
+++ src/router/index.js
@@ -70,6 +70,41 @@ const routes = [
     component: Huangshan
   },
   {
+    path: '/huangshan/tangjiazhuang',
+    name: 'Tangjiazhuang',
+    component: () => import('../views/locations/huangshan/Tangjiazhuang.vue')
+  },
+  {
+    path: '/huangshan/taipinghu',
+    name: 'Taipinghu',
+    component: () => import('../views/locations/huangshan/Taipinghu.vue')
+  },
+  {
+    path: '/huangshan/xianyuanju',
+    name: 'Xianyuanju',
+    component: () => import('../views/locations/huangshan/Xianyuanju.vue')
+  },
+  {
+    path: '/huangshan/huzhu',
+    name: 'Huzhu',
+    component: () => import('../views/locations/huangshan/Huzhu.vue')
+  },
+  {
+    path: '/huangshan/yanwoli',
+    name: 'Yanwoli',
+    component: () => import('../views/locations/huangshan/Yanwoli.vue')
+  },
+  {
+    path: '/huangshan/xingfushijia',
+    name: 'Xingfushijia',
+    component: () => import('../views/locations/huangshan/Xingfushijia.vue')
+  },
+  {
+    path: '/huangshan/chalinchang',
+    name: 'Chalinchang',
+    component: () => import('../views/locations/huangshan/Chalinchang.vue')
+  },
+  {
     path: '/huizhou',
     name: 'Huizhou',
     component: Huizhou
---
+++ src/views/locations/huangshan/Tangjiazhuang.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="detail-page">
+    <div class="detail-container">
+      <h1>汤家庄民宿集群</h1>
+      <p>详细内容待补充...</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.detail-page {
+  width: 100%;
+  height: 100%;
+  background-color: #dfdcb8;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.detail-container {
+  text-align: center;
+  padding: 40px;
+  background: rgba(255, 255, 255, 0.9);
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  font-size: 32px;
+  color: #333;
+  margin-bottom: 20px;
+}
+
+p {
+  font-size: 18px;
+  color: #666;
+}
+</style>
